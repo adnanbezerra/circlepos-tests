69a6904703e48bbc16452a0c77bf47fd6933e256
adnanbezerra <adnanbezerra@proton.me>
2023-10-07 17:31:08 -0300

    feat(back): :sparkles: refactor to add methods and check if isbn is valid

--- back-end/isbn-parser.rb
+++ back-end/isbn-parser.rb
@@ -1,23 +1,39 @@
+def getFullIsbn(isbn)
+  sum = 0;
+
+  for i in 0...isbn.length
+    if(i % 2 == 0)
+      sum += (isbn[i].to_i * 1);
+    else
+      sum += (isbn[i].to_i * 3);
+    end
+  end
+
+  mod = sum % 10;
+  finalResult = 10 - mod;
+
+  return finalResult;
+end
+
+def isIsbnValid(isbn) 
+  if (isbn.length != 12) 
+    return false;
+  else
+     return true;
+  end
+end
+
 puts "Welcome to the ISBN3 barcode calculator!";
 puts "Insert the ISBN number: ";
 
 isbn = gets.chomp;
-sum = 0;
 
-for i in 0...isbn.length
-  if(i % 2 == 0)
-    sum += (isbn[i].to_i * 1);
-  else
-    sum += (isbn[i].to_i * 3);
-  end
+while !isIsbnValid(isbn) do
+  puts "Insert a valid ISBN number!";
+  puts "Try again.";
+  isbn = gets.chomp;
 end
 
-mod = sum % 10;
+fullIsbn = getFullIsbn(isbn);
 
-if (mod == 10)
-  finalResult = 0;
-else 
-  finalResult = 10 - mod;
-end
-
-puts "The full ISBN number is #{isbn}#{finalResult}";
+puts "The full ISBN number is #{isbn}#{fullIsbn}";
