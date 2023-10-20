be04ce8f4a5c7c287a78744cb1c130f08b2c1afb
Adnan Bezerra <adnanbezerra@proton.me>
2023-10-20 16:26:26 -0300

    feat(front): :sparkles: prepare main page to receive data from API

--- front-end-circlepos/src/pages/MainPage/MainPage.jsx
+++ front-end-circlepos/src/pages/MainPage/MainPage.jsx
@@ -1,6 +1,35 @@
+import { useEffect, useState } from "react";
+import { Container, SecondTitle, Title } from "./MainPageStyle";
+import useGetBooksFromAPI from "../../actions/useGetBooksFromAPI";
+
 export default function MainPage() {
+  const { data: listFromApi } = useGetBooksFromAPI();
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    if (JSON.stringify(listFromApi) !== JSON.stringify([{}])) {
+      setBooks(listFromApi.results)
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [listFromApi]);
+
+  function getBooks() {
+    return (
+      books.map(entry => {
+        return (
+          <>
+          </>
+        )
+      })
+    )
+  }
+
   return (
-    <>
-    </>
+    <Container>
+      <Title>Welcome to CirclePOS store!</Title>
+      <SecondTitle>Here goes a list of our main books:</SecondTitle>
+
+      {getBooks}
+    </Container>
   )
 }
---
+++ front-end-circlepos/src/pages/MainPage/MainPageStyle.jsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  width: 100%;
+  padding: 25px 15%;
+  box-sizing: border-box;
+  flex-direction: column;
+`
+
+export const Title = styled.h1`
+  color: #000;
+  font-size: 20px;
+`
+
+export const SecondTitle = styled.h2`
+  margin-top: 20px;
+`
