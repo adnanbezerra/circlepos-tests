94499c0f37f2b6b81fde5c570a54ed5dfc438708
adnanbezerra <adnanbezerra@proton.me>
2023-11-04 21:32:22 -0300

    feat(front): :sparkles: add context to store cart for user

--- front-end-circlepos/src/App.jsx
+++ front-end-circlepos/src/App.jsx
@@ -3,10 +3,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from "./pages/MainPage/MainPage";
 import TopBar from "./components/TopBar/TopBar";
 import BottomBar from "./components/BottomBar/BottomBar";
+import { useState } from "react";
+import { CartContext } from "./context/CartContext";
 
 function App() {
+  const [cart, setCart] = useState([]);
+
   return (
-    <>
+    <CartContext.Provider value={{ cart, setCart }}>
       <GlobalStyle />
 
       <BrowserRouter>
@@ -18,7 +22,7 @@ function App() {
 
         <BottomBar />
       </BrowserRouter>
-    </>
+    </CartContext.Provider>
   )
 }
 
--- front-end-circlepos/src/components/TopBar/TopBar.jsx
+++ front-end-circlepos/src/components/TopBar/TopBar.jsx
@@ -1,14 +1,20 @@
 import { Link } from "react-router-dom";
 import { Container, Logo } from "./TopBarStyle";
+import { useContext } from "react";
+import { CartContext } from "../../context/CartContext";
 
 export default function TopBar() {
+  const { cart } = useContext(CartContext);
+
   return (
     <Container>
       <Link to={'/'}>
         <Logo src="https://circlepos.com/wp-content/uploads/2020/05/circlepos.com-normal.png" alt="" />
       </Link>
 
-      CART
+      <Link to={'/my-cart'} style={{ "textDecoration": "none", "color": "#fff" }}>
+        CART ({cart.length})
+      </Link>
     </Container>
   )
 }
