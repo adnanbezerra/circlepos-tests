8e2d8d283f424e94b44944d8d89e884229299b72
adnanbezerra <adnanbezerra@proton.me>
2023-11-06 21:15:29 -0300

    fix(front): :bug: buy cart hook wouldn't work properly

--- front-end-circlepos/src/actions/useBuyCart.jsx
+++ front-end-circlepos/src/actions/useBuyCart.jsx
@@ -3,26 +3,27 @@ import { API_URL } from '../mock/apiUrl';
 import { useState } from 'react';
 
 function useBuyCart() {
-  const [success, setSucess] = useState();
-  const [error, setError] = useState();
+  const [success, setSuccess] = useState([]);
 
   async function act(cart) {
     cart.forEach(async entry => {
       await postToAPI(entry);
     })
+
+    return success.length === cart.length;
   }
 
   async function postToAPI(payload) {
     axios.post(`${API_URL}/books/${payload.id}/purchase`)
       .then(response => {
-        setSucess(response.data);
+        setSuccess([...success, response.data]);
       })
       .catch(error => {
-        setError(error);
+        alert(`There was an error in your purchase! Error message: ${error.message}`);
       });
   }
 
-  return { act, success, error };
+  return { act };
 }
 
 export default useBuyCart;
